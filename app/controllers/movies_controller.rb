331a70d7a14df0f50468eb1e0b894493257b5633
Robert Marks <robert.j.marks@berkeley.edu>
2012-11-26 21:40:22 -0800

    added in page caching for index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
-
+ 
+ caches_page :index
+ 
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
